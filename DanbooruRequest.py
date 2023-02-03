7e03c9098b29932289319e4ff2d7d15b943143fd
ConstruKction <patdvkc@gmail.com>
2023-02-03 01:02:37 +0100

    Danbooru request class.

---
+++ DanbooruRequest.py
@@ -0,0 +1,28 @@
+import json
+import logging
+
+import requests
+
+DANBOORU_API_URL_TEMPLATE = "https://danbooru.donmai.us/posts.json?tags=%s&limit=%s&page=%s"
+
+
+class DanbooruRequest:
+    def __init__(self, tags, count, page_number):
+        self.page_number = page_number
+        self.api_url = DANBOORU_API_URL_TEMPLATE % (self.create_tags_string(tags), count, self.page_number)
+
+    def get_json(self):
+        response_json = json.loads(requests.get(self.api_url).text)
+        if len(response_json) == 0:
+            logging.info("No more posts found. Finished.")
+            return
+
+        return response_json
+
+    @staticmethod
+    def create_tags_string(tags):
+        tags_string = ''
+        for tag in tags:
+            tags_string = f"{tags_string}{tag}+"
+
+        return tags_string
