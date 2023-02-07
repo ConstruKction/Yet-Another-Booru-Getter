50baad6524bab76b423651f14819e15c6ab89225
ConstruKction <patdvkc@gmail.com>
2023-02-07 21:02:54 +0100

    Create ZerochanUser that initializes the user by reading configs from the config file

---
+++ users/ZerochanUser.py
@@ -0,0 +1,27 @@
+import json
+import logging
+import pathlib
+import sys
+
+from users.UserInterface import UserInterface
+
+
+class ZerochanUser(UserInterface):
+    def __init__(self):
+        self.z_id = self.initialize_user().get('z_id')
+        self.z_hash = self.initialize_user().get('z_hash')
+
+    def initialize_user(self):
+        zerochan_config_location = f"{pathlib.Path().resolve()}/config/zerochan.json"
+
+        if not pathlib.Path(zerochan_config_location).exists():
+            logging.critical(
+                f"zerochan.json config file not found!\n Get one from here, fill out your z_id and z_hash and put it "
+                f"in a folder named 'config': "
+                f"https://github.com/ConstruKction/Yet-Another-Booru-Getter/blob/master/config/zerochan.json")
+            sys.exit()
+
+        f = open(zerochan_config_location)
+        json_config = json.load(f)
+
+        return json_config
