03bf414aaf4cd91dd4d52cce72d6d30e885952be
ConstruKction <patdvkc@gmail.com>
2024-07-24 00:30:29 +0200

    Added type hinting for json_cleaner.py

--- json_cleaner.py
+++ json_cleaner.py
@@ -2,11 +2,11 @@ import re
 
 
 class JSONCleaner:
-    def __init__(self, dirty_json, regular_expressions):
+    def __init__(self, dirty_json: str, regular_expressions: dict):
         self.dirty_json = dirty_json
         self.regular_expressions = regular_expressions
 
-    def clean_json(self):
+    def clean_json(self) -> str:
         json = self.dirty_json
         for regular_expression, substitution in self.regular_expressions.items():
             json = re.sub(regular_expression, substitution, json)
